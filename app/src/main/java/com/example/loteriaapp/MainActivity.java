62ebb93c3f97452b7747ea7a34241c23cf885001
Ray <rafaelgame2012@gmail.com>
2021-03-31 17:08:29 -0600

    Quinto commit, algunas cosas que añadir

--- app/src/main/java/com/example/loteriaapp/MainActivity.java
+++ app/src/main/java/com/example/loteriaapp/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.loteriaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-public class MainActivity extends AppCompatActivity {
 
+//Importaciones de AddMob
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.InterstitialAd;
+//Fin de las implementaciones de AddMob
+
+public class MainActivity extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,3 +24,31 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
+    public void initAdds(){
+        //Empieza AddMob
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        //Banner
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        //Fin del banner
+
+        //Intersitital
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        //Fin del intersitital
+
+
+        //Termina AddMob
+    }
